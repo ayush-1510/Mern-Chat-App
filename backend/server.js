cc14e6c7190d1febc12de167a123d88e389a28a4
Ayush Srivastava <ayush.alp15@gmail.com>
2023-10-23 09:57:02 +0530

    Deploying

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound,errorHandler } = require("./middleware/errorMiddleware");
 const { Socket } = require("socket.io");
+const path=require('path');
 
 
 dotenv.config();
@@ -24,23 +25,23 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// // --------------------------deployment------------------------------
+// --------------------------deployment------------------------------
 
-// const __dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
-// // --------------------------deployment------------------------------
+// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);

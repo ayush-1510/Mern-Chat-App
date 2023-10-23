690ec5a57fc83a04b8fcb1479742bc220b852bf1
Ayush Srivastava <ayush.alp15@gmail.com>
2023-10-23 10:22:19 +0530

    createroot to ReactDOM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  
-  <BrowserRouter>
-    <ChatProvider>
-      <ChakraProvider>
+ReactDOM.render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <ChatProvider>
         <App />
-      </ChakraProvider>
       </ChatProvider>
     </BrowserRouter>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
